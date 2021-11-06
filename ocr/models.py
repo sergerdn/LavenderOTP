d75af07e48a27ff182d1c6cfc44f2a800e26745a
sergerdn <64213648+sergerdn@users.noreply.github.com>
2021-11-06 19:43:31 +0300

    feat: refactoring (#7)
    feat: refactoring code

---
+++ ocr/models.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, ValidationError, validator
+
+
+class OtpEntry(BaseModel):
+    secret: str
+    name: str
+    issuer: str
+    algorithm: int
+    digits: int
+    type: int
+
+    @validator('secret')
+    def secret_must_be_right(cls, v):
+        if len(v) != 24:
+            raise ValidationError("secret length must be 24 chairs, got length: %d" % len(v))
+        if v != v.upper():
+            raise ValidationError("password must be alpha(upper) numeric")
---
+++ ocr/worker.py
@@ -0,0 +1,126 @@
+import base64
+import glob
+import logging
+import os
+from typing import Union
+from urllib.parse import urlparse, parse_qs
+
+from PIL import Image
+from PIL import UnidentifiedImageError
+from pyzbar import pyzbar
+
+from ocr.models import OtpEntry
+from proto.v1.gauth_pb2 import MigrationPayload
+
+logger = logging.getLogger("[lavender-otp]")
+
+
+class LavenderOcrWorker(object):
+    src_dir: str
+    found = False
+    filename_ocr = []
+
+    def __init__(self, src_dir: str, dst_dir: str):
+        self.src_dir = src_dir
+        self.dst_dir = dst_dir
+
+    def ocr(self):
+        self.src_dir = self._norm_dir(self.src_dir)
+        if not self.src_dir:
+            return
+        self.dst_dir = self._norm_dir(self.dst_dir)
+        if not self.dst_dir:
+            return
+
+        for filename in self._get_files():
+            for data in self._get_text(filename):
+                self.found = True
+                dst_filename = os.path.join(self.dst_dir, "%s.txt" % os.path.basename(filename))
+                with open(dst_filename, "wb") as fp:
+                    fp.write(data)
+                    fp.write(b"\n")
+                logger.debug(f"wrote data from file: {os.path.basename(filename)}")
+                self.filename_ocr.append(dst_filename)
+
+        return self.found
+
+    def otp(self):
+        self.found = False
+        for filename in self.filename_ocr:
+            with open(filename, "rb") as fp:
+                for line in fp:
+                    line = line.strip()
+                    parsed_url = urlparse(line)
+                    params = parse_qs(parsed_url.query)
+                    data = params.get(b"data")
+                    if not data or len(data) == 0:
+                        logger.error(f"wrong data: {line}")
+                        continue
+
+                    data_encoded = data[0]
+                    data = base64.b64decode(data_encoded)
+                    payload = MigrationPayload()
+                    payload.ParseFromString(data)
+                    dst_filename = os.path.join(self.dst_dir, "%s.accounts.txt" % os.path.basename(filename))
+                    with open(dst_filename, "w") as fp:
+                        for item in payload.otp_parameters:
+                            self.found = True
+
+                            secret = str(base64.b32encode(item.secret), 'utf-8').replace('=', '')
+                            try:
+                                entry = OtpEntry(
+                                    secret=secret,
+                                    name=item.name,
+                                    issuer=item.issuer,
+                                    algorithm=item.algorithm,
+                                    digits=item.digits,
+                                    type=item.type
+                                )
+                            except ValueError:
+                                logger.error("invalid entry: %s", item)
+                                continue
+
+                            fp.write(f"#################################\n")
+                            fp.write(f"secret: {entry.secret}\n")
+                            fp.write(f"name: {entry.name}\n")
+                            fp.write(f"issuer: {entry.issuer}\n")
+                            fp.write(f"algorithm: {entry.algorithm}\n")
+                            fp.write(f"digits: {entry.digits}\n")
+                            fp.write(f"type:: {entry.type:}\n")
+                            fp.flush()
+
+        return self.found
+
+    def _norm_dir(self, src_dir) -> Union[str, bool]:
+        if not os.path.isabs(src_dir):
+            src_dir = os.path.abspath(src_dir)
+        src_dir = os.path.normpath(src_dir)
+
+        logger.debug(f"resolved dir to: {src_dir}")
+        if not os.path.exists(src_dir) or not os.path.isdir(src_dir):
+            logger.error(f"doesn't exists or not dir: {src_dir}")
+            return False
+
+        return src_dir
+
+    def _get_files(self):
+        for filename in glob.glob(os.path.join(self.src_dir, "*.*")):
+            if filename == ".gitignore":
+                continue
+            filename = os.path.join(self.src_dir, filename)
+            yield filename
+
+    def _get_text(self, filename):
+        try:
+            img = Image.open(filename)
+        except UnidentifiedImageError as ex:
+            logger.error(ex)
+            return
+
+        output = pyzbar.decode(img)
+        if len(output) == 0:
+            logger.warning(f"skipped because can't decoded: {filename}")
+            return
+        for one in output:
+            if one.data.startswith(b"otpauth-migration://"):
+                yield one.data
